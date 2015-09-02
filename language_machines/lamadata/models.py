60618aef196c31339d75f93b8316b19c89a212e7
Maarten van Gompel <proycon@anaproy.nl>
2015-09-02 22:02:41 +0200

    license name fix

--- language_machines/lamadata/models.py
+++ language_machines/lamadata/models.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import random
 
 PERSON_FUNCTIONS = { 'head':'Principal Investigator','phd':'PhD Candidate','postdoc':'Postdoc','prof':'Professor','assistantprof':'Assistant Professor','programmer':'Scientific Programmer','intern':'Intern','master':'Master\'s Candidate', 'bachelor':'Bachelor\'s Candidate', 'guest':'Guest Researcher','sysadmin':'System Administrator' }
-SOFTWARE_LICENSES = { 'gpl3': 'GNU Public License v3', 'gpl2': 'GNU Public License v2', 'agpl':'GPL Affero Public License v3','lgpl': 'Lesser GNU Public License v3', 'mit': 'MIT License', 'apache':'Apache License v2.0' }
+SOFTWARE_LICENSES = { 'gpl3': 'GNU Public License v3', 'gpl2': 'GNU Public License v2', 'agpl':'GNU Affero Public License v3','lgpl': 'Lesser GNU Public License v3', 'mit': 'MIT License', 'apache':'Apache License v2.0' }
 
 
 
